c2ace3bfaf9f3f971851502fe375a8ad69745ed9
H4438 <www.hao2912@gmail.com>
2023-12-01 02:43:35 +0000

    added: uet

---
+++ common_crawl/spiders/uet.py
@@ -0,0 +1,68 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+import sys
+import jsonlines as jsonl
+from bs4 import BeautifulSoup
+from scrapy.loader import ItemLoader
+sys.path.append("./")
+from items import CommonCrawlItem
+
+class UETConfig:
+
+  def __init__(self):
+    self.select_idx = 0
+    self.title_css = [".single-content-title",\
+     "#content strong"]
+    self.content_css = [".single-post-content-text",\
+     ".single-page-content"]
+
+  def get_config(self):
+    try:
+      title = self.title_css[self.select_idx]
+      content = self.content_css[self.select_idx]
+      self.select_idx += 1
+    except:
+      return None, None
+    return title, content
+
+class UETSpider(CrawlSpider):
+    name = "uet"
+    allowed_domains = ["uet.edu.edu.vn"]
+    start_urls = ["https://uet.vnu.edu.vn/"]
+    rules = [
+      Rule(LinkExtractor(allow=r"https://.*uet\.vnu\.edu\.vn/.*"),\
+       callback='parse_item', follow=True),
+    ]
+
+    def __init__(self, *a, **kw):
+      super(UETSpider, self).__init__(*a, **kw)
+      self.config = UETConfig()
+      print("Init")
+
+    @property
+    def header(self):
+      return {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+      }
+
+    def parse_item(self, response):
+      soup = BeautifulSoup(response.body, 'html.parser')
+      soup.attrs.clear()
+      item = ItemLoader(item=CommonCrawlItem())
+      title = None
+      content = None
+      # config
+      while(title == None or content == None):
+        title_css, content_css = self.config.get_config()
+        if title_css == None:
+          break
+        # get soup
+        title = soup.select_one(title_css)
+        content = soup.select_one(content_css)
+      item.add_value('id', response.url)
+      item.add_value('url', response.url)
+      item.add_value('title', title.text)
+      item.add_value('content', content.text)
+      item.add_value('html', str(content))
+      item.add_value('domain', str(self.allowed_domains[0]))
+      yield item.load_item()
